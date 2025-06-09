aab9bacd67a7bd1e98876dfd49efd56b159472de
pallandir <pallandir2@gmail.com>
2025-06-09 17:38:30 +0800

    Add Lazy and theme

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "auto", -- latte, frappe, macchiato, mocha
+        background = {
+          light = "latte",
+          dark = "mocha",
+        },
+        transparent_background = false,
+        show_end_of_buffer = false,
+        term_colors = false,
+        dim_inactive = {
+          enabled = false,
+          shade = "dark",
+          percentage = 0.15,
+        },
+        no_italic = false,
+        no_bold = false,
+        no_underline = false,
+      })
+      vim.cmd([[colorscheme catppuccin]])
+    end,
+  },
+}
