9e340c4615a3d5abdc47a55be57f442ff1fd545a
pallandir <pallandir2@gmail.com>
2025-06-10 01:16:49 +0800

    Update lazy config to detect and notify packages update

--- lua/lazy.lua
+++ lua/lazy.lua
@@ -11,4 +11,13 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins",{change_detection = {notify = false}})
+require("lazy").setup("plugins",{
+  checker = {
+    enabled = true, 
+    notify = true,
+  },
+  change_detection = {
+    notify = false,
+
+  },
+})
