f4f1569d83b7336f7c1daf01faed1cad193aaf0d
pallandir <pallandir2@gmail.com>
2025-06-11 22:01:14 +0800

    Attempt to fix lspconfig setup error

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -10,9 +10,6 @@ return {
     -- import lspconfig plugin
     local lspconfig = require("lspconfig")
 
-    -- import mason_lspconfig plugin
-    local mason_lspconfig = require("mason-lspconfig")
-
     -- import cmp-nvim-lsp plugin
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
@@ -77,7 +74,7 @@ return {
       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
     end
 
-    mason_lspconfig.setup_handlers({
+    require("mason-lspconfig").setup_handlers({
       function(server_name)
         lspconfig[server_name].setup({
           capabilities = capabilities,
