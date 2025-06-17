8e818310a77c9c4126e53746923d071967d3e5c5
pallandir <pallandir2@gmail.com>
2025-06-17 22:51:26 +0800

    Add new mapping to split terminal

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,6 +1,8 @@
 vim.g.mapleader = " "
 
 local keymap = vim.keymap
+local term_buf = nil
+local term_win = nil
 
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode by pressing jk" })
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highligh" })
@@ -21,3 +23,42 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close new tab" })
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in a new tab" })
+
+-- term
+vim.keymap.set({ "n", "t" }, "<leader>th", function()
+	-- If terminal is open, close it
+	if term_win and vim.api.nvim_win_is_valid(term_win) then
+		vim.api.nvim_win_close(term_win, true)
+		term_win = nil
+		return
+	end
+
+	-- Calculate 40% screen height
+	local height = math.floor(vim.o.lines * 0.4)
+
+	-- Open horizontal split with that height
+	vim.cmd(height .. "split")
+	term_win = vim.api.nvim_get_current_win()
+
+	-- Reuse existing terminal buffer or create a new one
+	if not term_buf or not vim.api.nvim_buf_is_valid(term_buf) then
+		term_buf = vim.api.nvim_create_buf(false, true)
+		vim.api.nvim_set_option_value("buflisted", false, { buf = term_buf })
+
+		-- Set the buffer in the window first
+		vim.api.nvim_win_set_buf(term_win, term_buf)
+
+		-- Then start the terminal
+		vim.fn.termopen(vim.o.shell, {
+			on_exit = function()
+				term_buf = nil
+				term_win = nil
+			end,
+		})
+	else
+		-- Buffer exists, just set it in the window
+		vim.api.nvim_win_set_buf(term_win, term_buf)
+	end
+
+	vim.cmd("startinsert")
+end, { desc = "Toggle horizontal terminal (40%)", noremap = true, silent = true })
